f9b0da442f304db8f4b6cfb2d808747937d2ad3e
m-amirazmi <60350614+m-amirazmi@users.noreply.github.com>
2022-12-08 03:00:13 +0800

    remove next theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { ThemeProvider } from 'next-themes';
 import { Quicksand } from '@next/font/google';
 import Layout from '../components/layout';
 import Head from 'next/head';
@@ -19,11 +18,9 @@ export default function App({ Component, pageProps }: AppProps) {
 				<title>Merazmi | Welcome To My Home</title>
 			</Head>
 			<main className="font-primary">
-				<ThemeProvider enableSystem={true} attribute="class">
-					<Layout>
-						<Component {...pageProps} />
-					</Layout>
-				</ThemeProvider>
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
 			</main>
 		</>
 	);
